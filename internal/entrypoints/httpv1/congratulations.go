231c9481b5317057bbfea950e3407a85cd6320aa
Xapsiel <ilyas_useinov3301@mail.ru>
2025-03-02 22:27:23 +0300

    finally? yes

--- internal/entrypoints/httpv1/congratulations.go
+++ internal/entrypoints/httpv1/congratulations.go
@@ -1,6 +1,8 @@
 package httpv1
 
 import (
+	"math/rand/v2"
+
 	"girls/internal/model"
 
 	"github.com/gofiber/fiber/v2"
@@ -14,6 +16,9 @@ type CongratulationResponse struct {
 func (r *Router) CongratulationsHandler(c *fiber.Ctx) error {
 	page := r.NewPage()
 	cong, err := r.service.ReadCongratulations()
+	rand.Shuffle(len(cong), func(i, j int) {
+		cong[i], cong[j] = cong[j], cong[i]
+	})
 	if err != nil {
 		return err
 	}
--- migrations/20250301115235_holiday.up.sql
+++ migrations/20250301115235_holiday.up.sql
@@ -1,8 +1,11 @@
-
-
 CREATE TABLE IF NOT EXISTS holidays(
-    id SERIAL PRIMARY KEY ,
-    text text NOT NULL ,
-    author varchar(100) NOT NULL DEFAULT 'Anonymus' ,
-    telegram_name  varchar(150) unique not null
+                                       id SERIAL PRIMARY KEY,
+                                       text TEXT NOT NULL,
+                                       author VARCHAR(100) NOT NULL DEFAULT 'Anonymus',
+                                       telegram_name VARCHAR(150) NOT NULL UNIQUE CHECK (telegram_name IN (
+                                                                                                           'Banan4ik_kmy', 'HeriofOmen', 'SaniySotkuVerni', 'Xapsiel', 'Zavsegdat1y',
+                                                                                                           'R7821699990', 'Metelykx', 'Whysirnik', 'Squerizz', 'F1rer0se',
+                                                                                                           'Timedust11', 'Dsedulya', 'Ssvivss', 'Beo_blod', 'Quinsone425',
+                                                                                                           'Why_n00tt', 'Bruhrayz', 'Letai_kak_ya', 'Selimqwe', 'Belyasha82', 'Myatiy_lox'
+                                           ))
 );
